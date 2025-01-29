97fdaee61d5e749a971f76add4821a7f5c363cbe
Surya Umapathy <32648210+suryaumapathy2812@users.noreply.github.com>
2025-01-29 11:57:50 +0530

    minor: added homepage seo metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Hero from "@/components/hero";
+import type { Metadata } from "next";
+import ScreenShot from "/public/og-screen-shot.jpg";
+
+export const metadata: Metadata = {
+  title: "Page Trail",
+  description:
+    "Understand your online activity by seeing exactly how much time you spend on each site, consolidated at the domain level, and optimize your browsing experience",
+  openGraph: {
+    images: [
+      {
+        url: ScreenShot.src,
+        width: 1920,
+        height: 1920,
+        alt: "Page Trail",
+      },
+    ],
+    title: "Page Trail",
+    description:
+      "Understand your online activity by seeing exactly how much time you spend on each site, consolidated at the domain level, and optimize your browsing experience",
+  },
+};
 
 export default function Page() {
   return (
